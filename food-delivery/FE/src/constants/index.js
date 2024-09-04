4d5a239834934845a348b1bd6cbbfa1159774dc8
Sureneee <168177455+Sureneee@users.noreply.github.com>
2024-09-04 20:15:05 +0800

    login form (#25)

---
+++ food-delivery/FE/src/constants/index.js
@@ -0,0 +1,17 @@
+import { Eye } from "lucide-react";
+
+
+export const LOGIN_INPUTS = [
+    {
+      placeholder: "Имэйл хаягаа оруулна уу",
+      name: "Имэйл",
+      type: "email",
+      
+    },
+    {
+      placeholder: "Нууц үг",
+      name: "Нууц үг",
+      type: "password",
+      eye: <Eye/>
+    },
+  ];
