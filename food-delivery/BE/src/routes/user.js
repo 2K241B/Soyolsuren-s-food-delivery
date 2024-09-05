0c6c97d7d0d42fead5830d30d98a8a1709fa8e36
Sureneee <soyolsurenbs@gmail.com>
2024-09-05 15:04:15 +0800

    update user

--- food-delivery/BE/src/routes/user.js
+++ food-delivery/BE/src/routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createUser, getAllUsers, getUser } from "../contrcoller/user.js";
+import { createUser, getAllUsers, getUser, updateUser } from "../contrcoller/user.js";
 
 const user = Router();
 
 user.post("/create", createUser);
 user.get(":id", getUser);
 user.get("/", getAllUsers);
+user.put("/:id", updateUser );
 
 export { user };
