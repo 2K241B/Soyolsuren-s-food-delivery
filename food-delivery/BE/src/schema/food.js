c55a4c8d8b820b832284782edf2d2a8cacc316a2
Soyolsuren <soyolsuren@Soyolsurens-MacBook-Air.local>
2024-09-13 13:31:00 +0800

    food schema

---
+++ food-delivery/BE/src/schema/food.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const foodSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  image: {
+    type: String,
+  },
+  ingeredient: {
+    type: String,
+  },
+  price: {
+    type: String,
+    required: [true, "Price is required"]
+  },
+  discount: {
+    type: Number,
+    default: 0,
+  },
+  categoryId: {
+    type: ''
+  }
+})
+
+export const FoodModel = model("food", foodSchema);
