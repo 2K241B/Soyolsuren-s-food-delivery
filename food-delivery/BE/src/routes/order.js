5456e012155b2aff2748631364c6dd5f429defd3
Sureneee <168177455+Sureneee@users.noreply.github.com>
2024-09-14 21:46:36 +0800

    Delete order query (#62)
    Co-authored-by: Soyolsuren <soyolsuren@Soyolsurens-MacBook-Air.local>

--- food-delivery/BE/src/routes/order.js
+++ food-delivery/BE/src/routes/order.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createOrder, getAllOrders } from "../contrcoller/order.js";
+import { createOrder, getAllOrders, deleteOrder } from "../contrcoller/order.js";
 
 
 const order = Router();
 
 order.post("/create", createOrder);
 order.get("/", getAllOrders);
+order.delete("/:id", deleteOrder );
 
 
 export { order };
