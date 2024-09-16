5fde58499a79061d9c0847a207bd666db133bcea
Sureneee <168177455+Sureneee@users.noreply.github.com>
2024-09-16 16:00:21 +0800

    [FE] Card component (#67)
    * Card component has error
    
    * card
    
    * Card component
    
    ---------
    
    Co-authored-by: Soyolsuren <soyolsuren@Soyolsurens-MacBook-Air.local>

--- food-delivery/FE/src/app/home/page.jsx
+++ food-delivery/FE/src/app/home/page.jsx
@@ -1,9 +1,21 @@
+
+
 import MainSection from "@/components/MainSection";
+import { Cards } from "@/components/Card";
+import  Card1  from "@/assets/Card1.png";
 
 const HomePage = () => {
     return (
         <div>
             <MainSection/>
+
+            <Cards
+            imageSrc={ Card1 }
+            discount={"20%"}
+            title={"Өглөөний хоол"}
+            price={14800}
+            discountedPrice={16800}
+            />
         </div>
     )
 
---
+++ food-delivery/FE/src/components/icon/CardsImage.jsx
@@ -0,0 +1,35 @@
+export const Card1 = () => {
+    return (
+        <div>
+            <img src="https://s3-alpha-sig.figma.com/img/669a/97ce/f4ad7e823b2a1cb020f7b7e74bce1ed7?Expires=1727049600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kKcd0o4FaRAnY2znhd~Rx2ZxvOw8upmmLBIksq~SHV64JwYCCmdTlSQs3mI9YauKYJ3SQCdKdrQpy9gpHYoPbSBT8L~O1GGsD-dCWqFPo36xIoHspGVFpWt9UsXNICnnw~eDfi0cb-OlA4rAzzrm2Bo9wsKEtYGgdDWCHJJiiyfnbL5rLJI6qbXHf4L-gHrtKGUJVVmVmhbDTpDiI9Zu7NXnxxGT2cLn9zrUrQzwZ-QqCOFu4JB6c7zep-uifEflahXYCUGkADOhJO2sNqKqsnj0RYYoum7jXh0x1e5y2jJ9GD2IYr2ObLVN3TU3YbvXlHSTrlenwJo3fVGZRMjOJA__">
+            </img>
+        </div>
+
+    )}
+
+export const Card2 = () => {
+    return (
+        <div>
+            <img src="https://www.womensweeklyfood.com.au/wp-content/uploads/sites/4/2018/10/18/29017/201501073773294094_2-scaled.jpg">
+            </img>
+        </div>
+
+    )}
+
+export const Card3 = () => {
+    return (
+        <div>
+            <img src="https://s3-alpha-sig.figma.com/img/27cb/2c3b/60df84dc4dd7d808ba224aff01eeb6d8?Expires=1727049600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=C-jTuUhwTXAXwyyLVViTPP0DpBRaQYnFGsgGMYhhVlKKaXJYtqaqAvrtjuPs5Kl6QANy01e~PkKmd0sv--yZO7ZvATs7RVWmx2YzNdH9ZpavCFLMAyAJq4ABcI~hPztApq~0rF7oJ7-Y0KWqYgxyXZ47K1vXnTZ7vsFtjcXq7sBNzZsY1LG2y1U9R9bopKnPENQTOJRwGZ7MdoviuuiaG4oZMRChb8IPS0fnFikT701zZxPpFXVFzkReTZNvv0YwSSJzTS9tPdbSqaT8jrDDx5XfWTYu1ro8xYgM2uhBjPB4qOcq4ZZsp-Ur2vK7aRnMjyT6Fpfo8Sbe-9zIQmcdpQ__">
+            </img>
+        </div>
+
+    )}
+
+export const Card4 = () => {
+    return (
+        <div>
+            <img src="https://s3-alpha-sig.figma.com/img/0765/ebe5/ba234ec6e3de87f32b816439af24885e?Expires=1727049600&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=E9VcoyLmipW8bzthTojfxSNoSHsQsgY6-vJpv5F~jHx9J42XMyEglKIXx7a1YFg20lYT0g1JRfjO54QndI4GXQFVisfA2d6tZtHuB~j4n2TWWNx-5JtHx4p9jTqYCrdMlhg9Phh9NKP-guPTVJVwrqCV~eDXzG9kP6Cgq2m8s5~rPlp7LAS25IIj4pX7ah1EL1tqUC8h6cjMJ3EL6V8l6~tHR-9KmIIFA87sLCIpOPiwljk0E8Q1jlGtcnWsyTZauTpv2NvCm92MPqI6YkFPM1kRSOM3zfDuFUSeqQjSjnUglsQT1otBTF8TFyTzmLw~XTZLJBMTwViXPUQu1WV9Mg___">
+            </img>
+        </div>
+
+    )}
