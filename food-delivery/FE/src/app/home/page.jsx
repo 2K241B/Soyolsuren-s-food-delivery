d55e851b2442ef819f731ca3048916b1f8e8281d
Zainab Jafari <zainabjafari998@gmail.com>
2024-09-17 13:02:34 +0200

    OrderRaport added

--- food-delivery/FE/src/app/home/page.jsx
+++ food-delivery/FE/src/app/home/page.jsx
@@ -1,8 +1,9 @@
 
-
+"use client"
 import MainSection from "@/components/MainSection";
 import { Cards } from "@/components/Card";
 import  Card1  from "@/assets/Card1.png";
+import OrdersReport from "@/components/OrdersReport";
 
 const HomePage = () => {
     return (
@@ -16,6 +17,7 @@ const HomePage = () => {
             price={14800}
             discountedPrice={16800}
             />
+            <OrdersReport />
         </div>
     )
 
