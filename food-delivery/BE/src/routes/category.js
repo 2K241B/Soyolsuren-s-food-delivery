57d1b3d11c8061ff2234964d1a89ef6707f6e750
Sureneee <168177455+Sureneee@users.noreply.github.com>
2024-09-18 16:52:42 +0800

    [BE] Get one category query (#84)

--- food-delivery/BE/src/routes/category.js
+++ food-delivery/BE/src/routes/category.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createCategory, getAllCategory } from "../contrcoller/category.js";
+import { createCategory, getAllCategory, getCategory } from "../contrcoller/category.js";
 
 
 const category = Router();
 
 category.post("/create", createCategory);
 category.get("/", getAllCategory);
+category.get("/:id", getCategory);
 
 
 export { category };
