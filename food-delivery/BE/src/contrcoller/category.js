5bb20a5246154fc751b8ec02b71451bba90469cf
Sureneee <soyolsurenbs@gmail.com>
2024-09-18 16:44:18 +0800

    [BE] Get all category query

--- food-delivery/BE/src/contrcoller/category.js
+++ food-delivery/BE/src/contrcoller/category.js
@@ -13,3 +13,14 @@ export const createCategory = async (req, res) => {
               res.status(500).send(error.message)
             }
           };
+
+
+export const getAllCategory = async (req, res) => {
+    try {
+        const response = await CategoryModel.find();
+        res.send(response);
+        } catch (error) {
+        console.error(error);
+        res.status(500).send(error.message);
+    }
+    };
