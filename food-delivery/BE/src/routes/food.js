38eef340bb14a96cf20ff1e77121cf0f13f764f6
Soyolsuren <soyolsuren@Soyolsurens-MacBook-Air.local>
2024-09-18 20:36:29 +0800

    [BE] Get one food query

--- food-delivery/BE/src/routes/food.js
+++ food-delivery/BE/src/routes/food.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createFood, getAllFood } from "../contrcoller/food.js";
+import { createFood, getAllFood, getFood } from "../contrcoller/food.js";
 
 
 const food = Router();
 
 food.post("/create", createFood);
 food.get("/", getAllFood);
+food.get("/:id", getFood);
 
 
 export { food };
