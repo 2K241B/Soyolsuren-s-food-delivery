5efd000cfda8f7ecac81e0713228f7d4383e6d62
Sureneee <soyolsurenbs@gmail.com>
2024-09-19 16:57:58 +0800

    [BE] Get categories with food query

--- food-delivery/BE/src/routes/category.js
+++ food-delivery/BE/src/routes/category.js
@@ -1,14 +1,16 @@
 import { Router } from "express";
-import { createCategory, deleteCategory, getAllCategory, getCategory, updateCategory } from "../contrcoller/category.js";
+import { createCategory, deleteCategory, getAllCategory, getCategoriesWithFoods, getCategory, updateCategory } from "../contrcoller/category.js";
 
 
 const category = Router();
 
 category.post("/create", createCategory);
 category.get("/", getAllCategory);
+category.get("/foods", getCategoriesWithFoods)
 category.get("/:id", getCategory);
 category.put("/:id", updateCategory);
 category.delete("/:id", deleteCategory );
+
 
 
 export { category };
