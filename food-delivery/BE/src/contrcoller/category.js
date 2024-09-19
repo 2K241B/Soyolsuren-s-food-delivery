e8654710608b4d2a09407f12e8df94305c6d82be
Sureneee <168177455+Sureneee@users.noreply.github.com>
2024-09-19 16:59:11 +0800

    [BE] Get categories with food query (#99)

--- food-delivery/BE/src/contrcoller/category.js
+++ food-delivery/BE/src/contrcoller/category.js
@@ -1,65 +1,88 @@
 import { CategoryModel } from "../schema/category.js";
 
-
 export const createCategory = async (req, res) => {
   const { name } = req.body;
- 
-  try {
-            const response = await CategoryModel.create({
-              name });
-              res.status(200).json(response);
-            } catch (error) {
-              console.log(error);
-              res.status(500).send(error.message)
-            }
-          };
 
+  try {
+    const response = await CategoryModel.create({
+      name,
+    });
+    res.status(200).json(response);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error.message);
+  }
+};
 
 export const getAllCategory = async (req, res) => {
-    try {
-        const response = await CategoryModel.find();
-        res.send(response);
-        } catch (error) {
-        console.error(error);
-        res.status(500).send(error.message);
-    }
-    };
+  try {
+    const response = await CategoryModel.find().populate("food");
+    res.send(response);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error.message);
+  }
+};
+
+export const getCategory = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const response = await CategoryModel.findById(id);
+    res.send(response);
+  } catch (error) {
+    console.error("error");
+    res.status(500).send(error.message);
+  }
+};
+
+export const getCategoriesWithFoods = async (req, res) => {
+  try {
+    const categories = await CategoryModel.aggregate([
+{
+    '$lookup': {
+    'from': 'foods',
+    'localField': '_id',
+    'foreignField': 'categoryId',
+    'as': 'foods'
+  }
+}
+])
+
+    return res.status(200).send(categories);
+  } catch (error) {
+    res.status(500).send({error});
+  }
+}
 
 
-export const getCategory = async (req, res ) => {
-    const { id } = req.params;
-    
-    try {
-        const response = await CategoryModel.findById( id );
-        res.send(response);
-      } catch (error) {
-        console.error("error");
-        res.status (500).send(error.message);
-      }
-    };
 
-export const updateCategory = async (req, res ) => {
-    const { id } = req.params;
-    const updatedCategory = req.body;
+export const updateCategory = async (req, res) => {
+  const { id } = req.params;
+  const updatedCategory = req.body;
 
-      try {
-            const response = await CategoryModel.findByIdAndUpdate( id, updatedCategory, { new: true } );
+  try {
+    const response = await CategoryModel.findByIdAndUpdate(
+      id,
+      updatedCategory,
+      { new: true }
+    );
 
-          res.send(response);
-        } catch (error) {
-          console.error("error");
-          res.status (500).send(error.message);
-        }
-      };
+    res.send(response);
+  } catch (error) {
+    console.error("error");
+    res.status(500).send(error.message);
+  }
+};
 
-export const deleteCategory = async (req, res ) => {
-    const { id } = req.params;
-        
-      try {
-          const response = await CategoryModel.findByIdAndDelete( id );
-          res.send(response);
-        } catch (error) {
-          console.error("error");
-          res.status (500).send(error.message);
-          }
-        };
+export const deleteCategory = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const response = await CategoryModel.findByIdAndDelete(id);
+    res.send(response);
+  } catch (error) {
+    console.error("error");
+    res.status(500).send(error.message);
+  }
+};
--- food-delivery/BE/src/schema/category.js
+++ food-delivery/BE/src/schema/category.js
@@ -3,8 +3,12 @@ import { Schema, model } from "mongoose";
 const categorySchema = new Schema({
   name: {
     type: String,
-    required: [true, "Name is required"]
-  }
+    required: [true, "Name is required"],
+  },
+  // foodId: {
+  //   type: Schema.ObjectId,
+  //   ref: "food",
+  // },
 });
 
 export const CategoryModel = model("category", categorySchema);
